1cf52ec4b1a9ede6700a6136745a298225f50064
Manikanta <nagasurimani6634@gmail.com>
2026-02-23 09:19:57 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Thank you for visiting my portfolio!");
+}
+
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
+}
